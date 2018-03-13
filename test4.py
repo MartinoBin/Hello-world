dd4b7b48f9af0582d84bffd47ad86143f3fbbf6c
Yuandao <36407106+MartinoBin@users.noreply.github.com>
2018-03-13 13:37:41 +0100

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,33 @@
+num=int(input('Enter a number:'))
+
+List=[]
+
+EnterList=list(range(1,num+1))
+
+for a in EnterList:
+    if num % a ==0:
+        List.append(a)
+
+print (List)
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    
